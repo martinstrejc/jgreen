2fc5b5a869b126f140014d284664375c27b83c64
strma17 <martin.strejc@ca.com>
2017-09-19 14:17:33 +0200

    Add missing Inject annotation in test

--- jgreen-core/src/test/java/cz/wicketstuff/jgreen/core/HelloWorldGlue.java
+++ jgreen-core/src/test/java/cz/wicketstuff/jgreen/core/HelloWorldGlue.java
@@ -20,6 +20,9 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cz.wicketstuff.jgreen.core.webdriver.HtmlService;
+
+import javax.inject.Inject;
+
 import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +32,7 @@ public class HelloWorldGlue  {
 
 	private static final Logger log = LoggerFactory.getLogger(HelloWorldGlue.class);
 
+	@Inject
 	private HtmlService html;
 
 	private String searchValue;
